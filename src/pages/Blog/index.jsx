f1c1e49f2b2de8aebf829412f2ce653f880c4b2f
Amoh-Ntim <amohntim123@gmail.com>
2023-10-28 22:03:33 +0000

    working on blog

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,7 +1,24 @@
-
+import { Navbar } from "../../components";
 
 export default function index() {
   return (
-    <div>Blog page</div>
+    <div>
+      <div>
+      <Navbar />
+      </div>
+
+      <div className="flex flex-col justify-center items-center mt-12">
+      <div className="text-green-200 font-semibold text-7xl italic">
+       News
+      </div>
+      <div className="font-bold text-3xl sm:flex sm:justify-center items-center">
+       Discover our latest blog <span className="">posts</span>
+      </div>
+      <div className="text-gray-600 font-sans flex flex-col justify-center items-center">
+        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum </div>
+        <div>tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</div>
+      </div>
+      </div>
+    </div>
   )
 }
