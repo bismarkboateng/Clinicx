b06d749d5759b6cd84e4fe9b353482d396b86a82
kwasii1 <kesbijnr@gmail.com>
2023-10-30 18:40:43 +0000

    First Commit
    About Page

--- src/components/Teamcard/index.jsx
+++ src/components/Teamcard/index.jsx
@@ -1,9 +1,19 @@
-
-
-export default function index() {
-  return (
-    <div>
-        Team card component
-    </div>
-  )
+export default function index({img,name,position,className}) {
+	return (
+		<>
+			<div className={`flex flex-col p-5 gap-y-2 ${className}`}>
+				<div className="flex flex-col p-5 gap-y-2 rounded-xl shadow-lg">
+					<img src={img} alt="" className={"rounded-xl "} />
+					<div className="flex-col flex gap-y-1">
+						<span className="text-center text-[#170F49] text-2xl font-bold">
+							{name}
+						</span>
+						<span className="text-center text-[#79dd97] uppercase font-bold tracking-wide">
+							{position}
+						</span>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
